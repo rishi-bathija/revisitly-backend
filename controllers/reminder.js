21f547ca096c48e44c7798ab41eced0e1e990c58
rishi-bathija <bathijarishi@gmail.com>
2025-08-01 11:37:54 +0530

    latest commit

--- controllers/reminder.js
+++ controllers/reminder.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
         user: process.env.MAIL_USER,
